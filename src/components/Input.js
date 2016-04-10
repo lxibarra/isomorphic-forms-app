d852cd4ea7aa08cad98c364707105cdc420881c6
Ricardo Ibarra <riibarra@stubhub.com>
2016-04-09 23:01:23 -0700

    r&d form validation

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,18 +2,37 @@ import React from 'react';
 
 class Input extends React.Component {
 
+  constructor() {
+    super();
+  }
+
+  componentWillMount() {
+    this.setState({
+      helpBlockCss:'help-block hide',
+      groupState:'form-group'
+    });
+  }
+
   updateModel(event) {
-      console.log(event.target.value);
-      this.props.model.setState({
-          [this.props.name]:event.target.value
-      });
+      let field = { name:event.target.name, value:event.target.value, valid:true };
+      //required validation must be moved to a mixin or inheritable class.
+      if(this.props.required && event.target.value.trim().length === 0) {
+          this.holder.classList.add('has-error');
+          field.valid = false;
+      } else {
+         this.holder.classList.remove('has-error');
+      }
+
+      this.props.modelSetter(field);
   }
 
   render() {
     return (
-      <div className="form-group">
-          <label htmlFor="input1">{this.props.label}</label>
-          <input id="input1" onChange={this.updateModel.bind(this)} name={this.props.name} type="text" className="form-control" placeholder={this.props.placeholder} />
+      <div ref={(ref) => this.holder = ref} className="form-group">
+          <label>{this.props.label}
+            <input id="input1" onChange={this.updateModel.bind(this)} name={this.props.name} type="text" className="form-control" placeholder={this.props.placeholder} />
+          </label>
+          <span ref={(ref)=> this.helpBlock = ref } className="help-block hide">Invalid input</span>
       </div>
     )
   }
--- src/pages/Venues.js
+++ src/pages/Venues.js
@@ -7,23 +7,50 @@ class Venues extends React.Component {
   componentWillMount() {
     this.setState({
       form:{
-
+        name:{},
+        email:{}
       }
     })
   }
 
+  setField(objectField) {
+    let form = this.state.form;
+    form[objectField.name] = objectField;
+    this.setState({
+      form:form
+    });
+
+    console.log(this.state.form);
+  }
+
   render() {
     //Must fnd a way to properly validate form
+    // The validation object maybe the desired validation structure
     return (
       <div>
-
-        <Input label="Name:" placeholder="Enter your name" model={this.state.form} />
-        <Input label="Email:" placeholder="Enter your email" model={this.state.form}/>
+        <Input
+          label="Name:"
+          name="name"
+          required={true}
+          validation={{
+            required:{
+                required:true,
+                msg:'This field is mandatory'
+            },
+            pattern:{
+              regex:/\d+/,
+              msg:'Pattern not met'
+            }}
+          }
+          placeholder="Enter your name"
+          modelSetter={this.setField.bind(this)}
+          />
+        <Input label="Email:" name="email" placeholder="Enter your email" modelSetter={this.setField.bind(this)} />
         <Button cssClass="success" label="Save"></Button>
         <hr/>
         <label>Current form values</label>
-        <p>{this.state.name}</p>
-        <p>{this.state.email}</p>
+        <p>{this.state.form.name.value}</p>
+        <p>{this.state.form.email.value}</p>
       </div>
     )
   }
